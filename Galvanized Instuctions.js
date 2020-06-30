ef443bc49dfe317390aaea1f2174b27571c57cd9
Scaarss1 <67659515+Scaarss1@users.noreply.github.com>
2020-06-30 17:25:01 -0400

    Add files via upload

---
+++ Galvanized Instuctions.js
@@ -0,0 +1,15 @@
+First, declare a variable named myArray and assign it to an empty array.
+Now populate myArray with two strings:
+Put your full name in the first string, and your favorite color in the second.
+Next, declare a function named cutName. It should expect a parameter name.
+cutName should return an array by breaking up the input string into individual words.
+Example: cutName("Douglas Crockford") should return ["Douglas", "Crockford"]
+Example: cutName("John B. Smith") should return ["John", "B.", "Smith"]
+Declare a new variable named myInfo and assign it to an empty object literal.
+Add the following three key-value pairs to myInfo:
+Key: fullName
+Value: The result of calling cutName on the name string within myArray.
+Key: favoriteColor
+Value: The color within myArray.
+Key: github
+Value:If you have a github handle, enter it here as a string. If not, set this to null instead.
